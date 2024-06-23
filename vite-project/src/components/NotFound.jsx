c8bd642a2f16668645f116c807f28446831573ea
deepak65190 <deepakbisht9773724383@gmail.com>
2024-06-23 22:53:53 +0530

    404 img added

--- vite-project/src/components/NotFound.jsx
+++ vite-project/src/components/NotFound.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const NotFound = () => {
   return (
     <div>
-    404  
+    <img src="https://img.freepik.com/free-vector/404-error-poster-with-page-found-use-website_1284-49339.jpg" alt="" />  
     </div>
   )
 }
