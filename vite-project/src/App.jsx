7353a8a632b7fca3257bd2aaec4547740b9d55a4
deepak65190 <deepakbisht9773724383@gmail.com>
2024-06-26 21:31:34 +0530

    e-com

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,19 +1,14 @@
-import AllRoutes from "./allRoutes" ;
-import Navbar from "./Navbar"; 
+import AllRoutes from "./allRoutes";
+import Navbar from "./Navbar";
 import CardCom from "./components/CardCom";
 
 function App() {
- 
-
-
   return (
     <>
-   <Navbar/>
-   <AllRoutes/>
+      <Navbar />
+      <AllRoutes />
     </>
-  )
+  );
 }
 
-export default App
-
-
+export default App;
